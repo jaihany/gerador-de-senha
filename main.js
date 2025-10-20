a33a048b0c6118078b64ef73f015f163f6110b72
jaihany <162121228+jaihany@users.noreply.github.com>
2025-10-20 09:33:36 -0300

    aa

--- main.js
+++ main.js
@@ -69,4 +69,10 @@ function diminuiTamanho(){
             forcaSenha.classlist.add('forte');
         } else if (entropia > 35  entropia < 57 );
         forcaSenha.classlist.add('media');
-    }
+        }else if (entropia <=35){
+            forcaSenha.classlist.add('fraca');
+        }
+        const valorEntropia = document.querySelector('.entropia');
+        {valorEntropia.textContent = "Um computador pode levar até " +
+        Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
+    } 
